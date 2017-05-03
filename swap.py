a40c647e462a942c389ff93452678dc663590058
BiiG <pedro.macias.gordaliza@gmail.com>
2017-05-03 09:23:28 +0200

    Introducimos swap function

---
+++ swap.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+def swap(a,b):
+  temp = a
+  a = b
+  b = temp
+  return a,b
+
+############test###########
+
+x= 1
+y = 9
+print "Las variables X e Y antes de intercamviar valores",x,y
+x,y = swap(x,y)
+print "Las variables X e Y antes de intercamviar valores",x,y
